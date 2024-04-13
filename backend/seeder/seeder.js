eb235787390385e5aa64762a3dc3ce6b98b3f9c2
miknivin <116654570+miknivin@users.noreply.github.com>
2024-04-13 23:27:32 +0530

    Update seeder.js

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -3,7 +3,7 @@ import Products from '../models/product.js'
 import products from "./data.js"
 const seedProducts = async () => {
     try{
-        await mongoose.connect("mongodb+srv://nivinmongo23o:v29IZzzu5EGumrcg@cluster0.ltsaofl.mongodb.net/ecom?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.DB_URI)
 
         await Products.deleteMany();
         console.log('Products are deleted');
